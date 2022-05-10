99e6e36d8d7b12b6669c8057b4f5edeab7788ea1
Petra Medgyesy <petramedgyesy@gmail.com>
2022-05-10 20:54:21 +0200

    Raktár projekt

---
+++ RaktarInformatika0430/Models/AruKeres.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaktarInformatika0430.Models
+{
+    public class AruKeres
+    {
+        public string Megnevezes { get; set; }
+        public string Beszallito { get; set; }
+        public List<Aru> AruLista { get; set; }
+        public SelectList BeszallitoLista { get; set; }
+    }
+}
